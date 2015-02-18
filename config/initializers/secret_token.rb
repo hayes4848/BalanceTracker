ed52f894529594f1e093577ae16aeac03af55409
Andy Hayes <andythemanhayes@hotmail.com>
2015-02-17 18:25:08 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BalanceTracker::Application.config.secret_token = '18e1e40d18061932b72414fc32f8fef1dfd77c82e0c61ee9f46ce846619a5b8ceb8cab294552d92b60e0b22f3392f8002ff2f94d6609abc86cd7244c1021934c'
